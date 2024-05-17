c10d04de5569e6699427a7c46dbf97ac685b3873
Griffin Campbell <griffincampbell6@gmail.com>
2024-05-17 09:11:14 -0400

    chore: Remove Firebase configuration file from version control

--- podunk-web/src/app/core/firebase.config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCT87uLyJbJ6psR37CGxH0a3FN2HpxKWB8",
-    authDomain: "podunk-34548.firebaseapp.com",
-    projectId: "podunk-34548",
-    storageBucket: "podunk-34548.appspot.com",
-    messagingSenderId: "2627770486",
-    appId: "1:2627770486:web:aa341266336a2595f06757",
-    measurementId: "G-W4N9ZFE1DP"
-  };
