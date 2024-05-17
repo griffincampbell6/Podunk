44e96c221ac6b125bc155836c19071e1dd42768b
Griffin Campbell <griffincampbell6@gmail.com>
2024-05-17 12:34:11 -0400

    chore: Update PodunkUser model with additional fields

--- podunk-web/src/app/models/podunk-user.model.ts
+++ podunk-web/src/app/models/podunk-user.model.ts
@@ -1,3 +1,11 @@
 export interface PodunkUser {
-    displayName: string;
+    CREATED_TS: string;
+    DISPLAY_NAME: string;
+    DOB: Map<string, number>;
+    FIRST_NAME: string;
+    LAST_NAME: string;
+    LOCATION: Map<string, string>
+    PRONOUNS: string;
+    UPDATED_TS: string;
+    USER_UID: string;
 }
