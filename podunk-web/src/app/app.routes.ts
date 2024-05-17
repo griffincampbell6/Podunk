0701c5f39cf7f907c26c12892877608a085cda59
Griffin Campbell <griffincampbell6@gmail.com>
2024-05-17 15:01:51 -0400

    feat: Add PodunkAccountFormComponent for user registration

--- podunk-web/src/app/app.routes.ts
+++ podunk-web/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PodunkAccountFormComponent } from './user-registration/account-form/account-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'podunk-account-form', component: PodunkAccountFormComponent }
+];
